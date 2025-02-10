83e1064fdb767f51b7b78f4b4db9915031d39e04
Mels <mels.vagharshyan18@gmail.com>
2025-02-10 14:49:35 +0400

    current commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DATABASE_URL as string),
+    MongooseModule.forRoot(
+      'mongodb+srv://melsvagharshyan18:mels7878@cluster0.jedxf.mongodb.net',
+    ),
     MessagesModule,
   ],
   controllers: [AppController],
